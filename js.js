1a5b6641bc55d78bb8a586338c1258bff88b7579
jperez-2022076 <134990595+jperez-2022076@users.noreply.github.com>
2024-01-18 17:07:55 -0600

    tercera entrega

---
+++ js.js
@@ -0,0 +1,21 @@
+
+
+    let letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","z"];
+    var oportunidad = 5;
+    
+   const divelementos = document.createDocumentFragment();
+   let conBotones = document.getElementById("divLetras");
+
+        for( let letra of letras){
+            
+            const boton = document.createElement("button");
+            boton.textContent = letra;
+            boton.value = letra;
+            boton.id = "botton" + letra;
+           // boton.addEventListener("click",boton);
+            divelementos.appendChild(boton);
+          
+        };
+        conBotones.appendChild(divelementos);
+
+
